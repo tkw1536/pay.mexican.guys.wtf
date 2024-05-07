806d11ccc5a8ee07ef9d88909959550c1fd7ab0f
Kai Amann <kai.amann@fau.de>
2024-05-07 14:14:34 +0200

    Give verbose names to model

--- pay_mexican_guys/models.py
+++ pay_mexican_guys/models.py
@@ -5,6 +5,10 @@ from django.forms import ValidationError
 class WhoPays(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = "Who pays?"
+        verbose_name_plural = "Who pays?"
+
     def save(self, *args, **kwargs):
         if not self.pk and WhoPays.objects.exists():
             raise ValidationError('Only one person can pay the mexicans')
