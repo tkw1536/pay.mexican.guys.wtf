7f977224fdcfd0f72e993d46c0fa357ed6f3804a
Kai Amann <kai.amann@fau.de>
2024-12-14 05:55:47 +0000

    Change url to next and adjust template

--- pay_mexican_guys/urls.py
+++ pay_mexican_guys/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import index,  rotate
+from .views import index, rotate
 
 urlpatterns = [
     path('', index),
-    path('rotate/', rotate),
+    path('next/', rotate),
     path('admin/', admin.site.urls),
 ]
